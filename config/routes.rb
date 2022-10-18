db4f35e95cec38a66601087e9bf4948b5547c56b
Minion <lakshmajim@users.noreply.github.com>
2022-10-18 18:24:04 +0530

    company_employees model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :company_employees
   devise_for :employees
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
