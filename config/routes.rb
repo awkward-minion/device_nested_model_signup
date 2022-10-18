5844a5246ae0fa5c6e9afbbb96f2af709ac985e4
Minion <lakshmajim@users.noreply.github.com>
2022-10-18 18:32:52 +0530

    custom signup controller for employee model

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :company_employees
-  devise_for :employees
+  devise_for :employees, controllers: {
+    registrations: 'employee/registrations',
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
